92d3a7eee55e2a2c49cf3b4b9910eef6c64b956a
Riaz <127931061+riaz-gain@users.noreply.github.com>
2024-12-19 14:24:19 +0600

    Update players.js

--- src/helpers/players.js
+++ src/helpers/players.js
@@ -18,5 +18,5 @@ export const initialPlayers = [
   { id: 17, name: "Rashed", weight: 1 },
   { id: 18, name: "Arif", weight: 1 },
   { id: 19, name: "Sakib", weight: 1 },
-  { id: 20, name: "Mominul", weight: 1 },Ï
+  { id: 20, name: "Mominul", weight: 1 }
 ];
