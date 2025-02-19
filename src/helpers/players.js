b455dca7643c5786a267b80e4d0c9c4bce7bd0bc
Riaz <127931061+riaz-gain@users.noreply.github.com>
2025-02-19 15:24:26 +0600

    Update players.js

--- src/helpers/players.js
+++ src/helpers/players.js
@@ -6,12 +6,13 @@ export const initialPlayers = [
   { id: 9, name: "Elias", weight: 3 },
   { id: 11, name: "Mezanur", weight: 3 },
   { id: 12, name: "Kamruzzaman", weight: 4 },
-  { id: 13, name: "Evan", weight: 4 },
   { id: 14, name: "Shahnewaz", weight: 1 },
   { id: 15, name: "Tahsin", weight: 1 },
   { id: 16, name: "Mizan", weight: 2 },
   { id: 17, name: "Rashed", weight: 1 },
   { id: 18, name: "Arif", weight: 1 },
   { id: 19, name: "Saqib", weight: 1 },
-  { id: 20, name: "Momenul", weight: 1 }
+  { id: 20, name: "Momenul", weight: 2 }
+  { id: 21, name: "Nasim", weight: 3 }
+  { id: 22, name: "Rifat", weight: 1 }
 ];
