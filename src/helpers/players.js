69128c5a67f51fa43320290bd299d53fc0ec4fd8
Riaz <127931061+riaz-gain@users.noreply.github.com>
2025-04-07 20:16:40 +0600

    Update players.js

--- src/helpers/players.js
+++ src/helpers/players.js
@@ -1,7 +1,6 @@
 export const initialPlayers = [
   { id: 1, name: "Riaz", weight: 3 },
-  { id: 3, name: "Rahat", weight: 3 },
-  { id: 5, name: "Zabid", weight: 1 },
+  { id: 5, name: "Zabid", weight: 2 },
   { id: 6, name: "Rakib", weight: 4 },
   { id: 9, name: "Elias", weight: 3 },
   { id: 11, name: "Mezanur", weight: 3 },
@@ -9,10 +8,11 @@ export const initialPlayers = [
   { id: 14, name: "Shahnewaz", weight: 1 },
   { id: 15, name: "Tahsin", weight: 1 },
   { id: 16, name: "Mizan", weight: 2 },
-  { id: 17, name: "Rashed", weight: 1 },
+  { id: 17, name: "Rashed", weight: 2 },
   { id: 18, name: "Arif", weight: 1 },
   { id: 19, name: "Saqib", weight: 1 },
   { id: 20, name: "Momenul", weight: 2 },
-  { id: 21, name: "Nasim", weight: 3 },
-  { id: 22, name: "Rifat", weight: 1 }
+  { id: 22, name: "Rifat", weight: 1 },
+   { id: 23, name: "Akbar", weight: 1 },
+ { id: 24, name: "Maruf", weight: 1 },
 ];
